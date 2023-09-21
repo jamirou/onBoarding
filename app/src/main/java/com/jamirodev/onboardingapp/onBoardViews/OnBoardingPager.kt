3c3b6524f143dda21109c501b415a7676a7033c6
Jamiro <jamirocristian15@gmail.com>
2023-09-21 19:07:39 -0300

    added the complete box for onboarding.

--- app/src/main/java/com/jamirodev/onboardingapp/onBoardViews/OnBoardingPager.kt
+++ app/src/main/java/com/jamirodev/onboardingapp/onBoardViews/OnBoardingPager.kt
@@ -2,9 +2,19 @@ package com.jamirodev.onboardingapp.onBoardViews
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.HorizontalPager
 import com.google.accompanist.pager.PagerState
@@ -19,8 +29,36 @@ fun OnBoardingPager(
 ) {
     Box(modifier = modifier) {
         Column(horizontalAlignment = Alignment.CenterHorizontally) {
-            HorizontalPager(state = pagerState) {
-                //CENTER
+            HorizontalPager(state = pagerState) { page ->
+                Column(
+                    modifier = Modifier
+                        .padding(60.dp)
+                        .fillMaxWidth(),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    DataLoader(
+                        modifier = Modifier
+                            .size(200.dp)
+                            .fillMaxWidth()
+                            .align(alignment = Alignment.CenterHorizontally),
+                        image = item[page].image
+                    )
+                    Text(
+                        text = item[page].title,
+                        modifier = Modifier
+                            .padding(top = 50.dp),
+                        color = Color.Black,
+                        style = MaterialTheme.typography.bodyLarge,
+                        fontWeight = FontWeight.Bold
+                    )
+                    Text(
+                        text = item[page].description,
+                        color = Color.Black,
+                        textAlign = TextAlign.Center,
+                        fontWeight = FontWeight.Light,
+                        fontSize = 20.sp
+                    )
+                }
             }
             PageIndicator(item.size, pagerState.currentPage)
         }
